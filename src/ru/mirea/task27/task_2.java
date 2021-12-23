39fce1fe02c77bf33b6da45a88907c22fef179ae
EvgeniyZharov <76780101+EvgeniyZharov@users.noreply.github.com>
2021-12-23 13:49:05 +0300

    Add files via upload

---
+++ src/ru/mirea/task27/task_2.java
@@ -0,0 +1,29 @@
+package com.company.Excercise_27;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class task_2 {
+    public static void main(String[] args) {
+        Map<String, ArrayList<String>> map = new HashMap<>();
+
+        ArrayList<String> city_russia = new ArrayList<>();
+        city_russia.add("Sochi");
+        city_russia.add("Moscow");
+        city_russia.add("Saint Petersburg");
+        map.put("Russia", city_russia);
+
+        ArrayList<String> citi_usa = new ArrayList<>();
+        citi_usa.add("New York");
+        citi_usa.add("Washington");
+        citi_usa.add("Nevada");
+        map.put("USA", citi_usa);
+
+        System.out.println(map);
+        System.out.println(map.get("Russia"));
+        System.out.println(map.get("USA"));
+
+    }
+}
